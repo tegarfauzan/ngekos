5cd159a7da2b09230321eb62db8d0468a34f66d1
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2024-10-15 17:20:31 +0700

    m

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+var categoriesSwiper = new Swiper("#Categories", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    slidesOffsetAfter: 20,
+    slidesOffsetBefore: 20,
+});
+var startYourSwiper = new Swiper("#PopularKosSlider", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    slidesOffsetAfter: 20,
+    slidesOffsetBefore: 20,
+});
