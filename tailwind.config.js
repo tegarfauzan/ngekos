90b1b0b890b9c2406ddb10cce5df31b5a412867b
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2024-10-16 12:11:07 +0700

    m

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/**/*.{html,js}"],
+    content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
             fontFamily: {
-                "poppins": ["Poppins", "sans-serif"],
+                poppins: ["Poppins", "sans-serif"],
+            },
+            colors: {
+                "ngekos-gray": "#5e6f76",
+                "ngekos-orange": "#ff801a",
+                "ngekos-almostwhite": "#f5f6f8",
             },
         },
     },
